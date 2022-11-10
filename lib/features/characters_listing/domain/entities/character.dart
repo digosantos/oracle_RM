f1938a0d2e7243b36814ba6de5563af27664d727
Digo <santos_ro@hotmail.com>
2022-11-10 15:57:20 -0300

    feat: create Character entity

---
+++ lib/features/characters_listing/domain/entities/character.dart
@@ -0,0 +1,13 @@
+class Character {
+  final String name;
+  final String imageUrl;
+  final String specie;
+  final int episodesAmount;
+
+  Character({
+    required this.name,
+    required this.imageUrl,
+    required this.specie,
+    required this.episodesAmount,
+  });
+}
