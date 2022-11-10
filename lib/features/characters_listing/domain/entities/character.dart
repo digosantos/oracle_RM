9b2eba3530f31f9b899959ad3ff1b0a48d20aab4
Digo <santos_ro@hotmail.com>
2022-11-10 16:52:51 -0300

    refactor: make Character entity equatable

--- lib/features/characters_listing/domain/entities/character.dart
+++ lib/features/characters_listing/domain/entities/character.dart
@@ -1,13 +1,18 @@
-class Character {
+import 'package:equatable/equatable.dart';
+
+class Character extends Equatable {
   final String name;
   final String imageUrl;
   final String specie;
   final int episodesAmount;
 
-  Character({
+  const Character({
     required this.name,
     required this.imageUrl,
     required this.specie,
     required this.episodesAmount,
   });
+
+  @override
+  List<Object?> get props => [name, imageUrl, specie, episodesAmount];
 }
