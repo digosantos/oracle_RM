98e93f50ad13b8e887896dda909a98d9378f4090
Digo <santos_ro@hotmail.com>
2022-11-10 18:41:34 -0300

    feat: create AppError class

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -8,3 +8,7 @@ abstract class Failure extends Equatable {
   @override
   List<Object?> get props => properties;
 }
+
+class AppError extends Failure {
+  const AppError({required super.properties});
+}
