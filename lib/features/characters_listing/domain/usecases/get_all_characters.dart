a07c5cb4c860e11d23fc513a6b379fd67ba2f0ed
Digo <santos_ro@hotmail.com>
2022-11-10 17:37:46 -0300

    feat: create getAllCharacters use case

---
+++ lib/features/characters_listing/domain/usecases/get_all_characters.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:oracle_rm/core/error/error.dart';
+import 'package:oracle_rm/features/characters_listing/domain/entities/character.dart';
+import 'package:oracle_rm/features/characters_listing/domain/repositories/characters_repository.dart';
+
+class GetAllCharacters {
+  final CharactersRepository charactersRepository;
+
+  GetAllCharacters({required this.charactersRepository});
+
+  Future<Either<Failure, List<Character>>> execute() async {
+    return await charactersRepository.getAllCharacters();
+  }
+}
