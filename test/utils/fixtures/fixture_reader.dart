977a686c7f752731a37e30e20597f659cdaefac9
Digo <santos_ro@hotmail.com>
2022-11-11 17:38:47 -0300

    style: adjust fixture_reader format

--- test/utils/fixtures/fixture_reader.dart
+++ test/utils/fixtures/fixture_reader.dart
@@ -1,3 +1,4 @@
 import 'dart:io';
 
-String fixture({required String name}) => File('test/utils/fixtures/$name').readAsStringSync();
+String fixture({required String name}) =>
+    File('test/utils/fixtures/$name').readAsStringSync();
