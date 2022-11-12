89e7dac427ef5fc5b14b3a7873c5aa3869848f56
Digo <santos_ro@hotmail.com>
2022-11-12 18:51:55 -0300

    feat: create BaseNetworkClient

---
+++ lib/core/network/base_network_client.dart
@@ -0,0 +1,15 @@
+import 'package:graphql/client.dart';
+import 'package:oracle_rm/core/error/exceptions.dart' as exception;
+
+class BaseNetworkClient {
+  final GraphQLClient client;
+
+  BaseNetworkClient({required this.client});
+
+  Future<Map<String, dynamic>> query({required String document, Map<String, dynamic>? params}) async {
+    final options = QueryOptions(document: gql(document), variables: params ?? {});
+    final result = await client.query(options);
+    if (result.hasException) throw exception.ServerException(message: 'Server Error');
+    return result.data!;
+  }
+}
