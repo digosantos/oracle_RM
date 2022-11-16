6516e0ec866b0cf46712be65d66f1c97b70b6e12
Digo <santos_ro@hotmail.com>
2022-11-16 12:32:03 -0300

    feat: create CharactersListingPage

--- lib/features/characters_listing/ui/pages/characters_listing_page.dart
+++ lib/features/characters_listing/ui/pages/characters_listing_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:oracle_rm/core/characters/ui/widgets/card.dart';
 import 'package:oracle_rm/features/characters_listing/ui/bloc/bloc.dart';
 
 import '../../../../core/injection_container.dart';
@@ -14,15 +15,26 @@ class CharactersListingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.greenAccent,
       appBar: AppBar(
         title: const Text('R&M Oracle'),
+        backgroundColor: Colors.purple,
+        elevation: 0,
       ),
       body: BlocBuilder<CharactersListingBloc, CharactersListingState>(
         bloc: charactersListBloc,
         builder: (context, state) {
           if (state is CharactersListLoadedState) {
-            return Center(
-              child: Text('Amount of characters: ${state.charactersList.length}'),
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 36),
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: state.charactersList.length,
+                itemBuilder: (context, index) {
+                  return CharacterCard(character: state.charactersList[index]);
+                },
+              ),
             );
           }
           return const Center(child: CircularProgressIndicator());
