2134e5974adcd155d30888bbc3700e75bf03fd21
Digo <santos_ro@hotmail.com>
2022-11-16 13:03:23 -0300

    style: organize imports

--- lib/features/character_details/ui/bloc/character_details_bloc.dart
+++ lib/features/character_details/ui/bloc/character_details_bloc.dart
@@ -1,24 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:oracle_rm/core/domain/usecases/usecase.dart';
 import 'package:oracle_rm/core/error/error.dart';
-import 'package:oracle_rm/features/character_details/ui/bloc/character_detail_state.dart';
-import 'package:oracle_rm/features/character_details/ui/bloc/character_details_event.dart';
+import 'package:oracle_rm/features/character_details/ui/bloc/bloc.dart';
 
 import '../../../../core/characters/domain/entities/entities.dart';
 import '../../../characters_listing/domain/usecases/usecases.dart';
 
-class CharacterDetailsBloc
-    extends Bloc<CharacterDetailsEvent, CharacterDetailsState> {
+class CharacterDetailsBloc extends Bloc<CharacterDetailsEvent, CharacterDetailsState> {
   final UseCase<Character, RequestedCharacter> getCharacterDetailsUseCase;
 
-  CharacterDetailsBloc({required this.getCharacterDetailsUseCase})
-      : super(InitialState()) {
+  CharacterDetailsBloc({required this.getCharacterDetailsUseCase}) : super(InitialState()) {
     on<GetCharacterDetailsEvent>(_loadCharacterDetails);
   }
 
   /// Callbacks
-  void _loadCharacterDetails(
-      GetCharacterDetailsEvent event, Emitter emit) async {
+  void _loadCharacterDetails(GetCharacterDetailsEvent event, Emitter emit) async {
     emit(LoadingState());
     final response = await getCharacterDetailsUseCase(event.requestedCharacter);
     emit(response.fold(
