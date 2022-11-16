3eb5ca6fd9aabe86ee26bb6cbecfd11f3da92d7b
Digo <santos_ro@hotmail.com>
2022-11-15 22:40:28 -0300

    refactor: tests to return appropriate response

--- test/core/characters/data/models/character_test.dart
+++ test/core/characters/data/models/character_test.dart
@@ -23,7 +23,7 @@ void main() {
       final Map<String, dynamic> characterMap = jsonDecode(
         fixture(name: '/characters/character.json'),
       );
-      final sut = CharacterModel.fromJson(characterMap['data']['character']);
+      final sut = CharacterModel.fromJson(characterMap['character']);
       expect(sut, rick);
     });
   });
