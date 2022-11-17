11ef29e125d97b3378cccefe85f7d55a65254277
Digo <santos_ro@hotmail.com>
2022-11-17 16:25:27 -0300

    feat: create CharacterDetails entity

---
+++ lib/core/characters/domain/entities/character_details.dart
@@ -0,0 +1,44 @@
+import 'package:oracle_rm/core/characters/domain/entities/entities.dart';
+
+import '../../../episodes/domain/entities/entities.dart';
+
+class CharacterDetails extends Character {
+  final String status;
+  final Origin origin;
+  final Location location;
+  final String createdAt;
+  final List<Episode> episodes;
+
+  const CharacterDetails({
+    required String id,
+    required String name,
+    required String imageUrl,
+    required String species,
+    required List<String> episodesIds,
+    required this.status,
+    required this.origin,
+    required this.location,
+    required this.createdAt,
+    required this.episodes,
+  }) : super(
+          id: id,
+          name: name,
+          imageUrl: imageUrl,
+          species: species,
+          episodesIds: episodesIds,
+        );
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+        imageUrl,
+        species,
+        episodesIds,
+        status,
+        origin,
+        location,
+        createdAt,
+        episodes,
+      ];
+}
