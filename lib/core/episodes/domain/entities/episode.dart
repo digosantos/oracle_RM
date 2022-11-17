68c1da3ad62758a6367160c31024e2412505203b
Digo <santos_ro@hotmail.com>
2022-11-17 16:23:00 -0300

    feat: add episode entity

--- lib/core/episodes/domain/entities/episode.dart
+++ lib/core/episodes/domain/entities/episode.dart
@@ -1,1 +1,11 @@
+import 'package:equatable/equatable.dart';
 
+class Episode extends Equatable {
+  final String name;
+  final String airDate;
+
+  const Episode({required this.name, required this.airDate});
+
+  @override
+  List<Object?> get props => [name, airDate];
+}
