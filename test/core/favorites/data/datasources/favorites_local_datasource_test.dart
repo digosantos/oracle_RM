1998f208a796834e738424397fd3837517f5019b
Digo <santos_ro@hotmail.com>
2022-11-20 14:22:57 -0300

    test: cover remove method from FavoritesLocalDataSource

--- test/core/favorites/data/datasources/favorites_local_datasource_test.dart
+++ test/core/favorites/data/datasources/favorites_local_datasource_test.dart
@@ -1,12 +1,9 @@
-import 'dart:convert';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:oracle_rm/core/favorites/data/datasources/datasources.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../../utils/faux.dart';
-import '../../../../utils/fixtures/fixture_reader.dart';
 
 @GenerateMocks([FavoritesLocalDataSource])
 void main() {
@@ -14,22 +11,16 @@ void main() {
   late FavoritesLocalDataSourceImpl favoritesLocalDataSourceImpl;
 
   final characterId = Faux.character.id;
-  final emptyFavorites = Map<String, Object>.from(jsonDecode(
-    fixture(name: '/favorites/empty_favorites.json'),
-  ));
-  final favoriteCharacters = Map<String, Object>.from(jsonDecode(
-    fixture(name: '/favorites/favorite_characters.json'),
-  ));
-
-  setUpAll(() async {
-    SharedPreferences.setMockInitialValues(emptyFavorites);
-    sharedPreferences = await SharedPreferences.getInstance();
-    favoritesLocalDataSourceImpl = FavoritesLocalDataSourceImpl(sharedPreferences: sharedPreferences);
-  });
 
   group('FavoritesLocalDataSourceImpl', () {
+    /// NOTE:
+    /// Due to the issue [https://github.com/flutter/flutter/issues/28837]
+    /// below tests had to be setup individually instead of using setUpAll
+    /// method. setMockInitialValues doesn't let us set new values.
     test('should save characterId', () async {
-      SharedPreferences.setMockInitialValues(emptyFavorites);
+      await setupPreferences(Faux.favoritesKey, []);
+      sharedPreferences = await SharedPreferences.getInstance();
+      favoritesLocalDataSourceImpl = FavoritesLocalDataSourceImpl(sharedPreferences: sharedPreferences);
       final sut = await favoritesLocalDataSourceImpl.save(characterId: characterId);
 
       expect(sut, true);
@@ -37,11 +28,20 @@ void main() {
     });
 
     test('should remove characterId', () async {
-      SharedPreferences.setMockInitialValues(favoriteCharacters);
+      await setupPreferences(Faux.favoritesKey, ['999', '2']);
+      sharedPreferences = await SharedPreferences.getInstance();
+      favoritesLocalDataSourceImpl = FavoritesLocalDataSourceImpl(sharedPreferences: sharedPreferences);
+
       final sut = await favoritesLocalDataSourceImpl.remove(characterId: characterId);
 
       expect(sut, true);
       expect(sharedPreferences.getStringList(Faux.favoritesKey), ['2']);
     });
   });
+}
+
+Future setupPreferences(String key, List<String> value) async {
+  SharedPreferences.setMockInitialValues(<String, Object>{'flutter.$key': value});
+  final preferences = await SharedPreferences.getInstance();
+  await preferences.setStringList(key, value);
 }
